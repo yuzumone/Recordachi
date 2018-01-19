e20efa85c4123f0c851a679bdcfcc346c7e4adc0
yuzumone <yuzumone@outlook.com>
2018-01-19 11:39:59 +0900

    Update event adapter

--- app/src/main/java/net/yuzumone/recordachi/view/BindingAdapter.kt
+++ app/src/main/java/net/yuzumone/recordachi/view/BindingAdapter.kt
@@ -2,6 +2,8 @@ package net.yuzumone.recordachi.view
 
 import android.databinding.BindingAdapter
 import android.widget.TextView
+import net.yuzumone.recordachi.R
+import net.yuzumone.recordachi.model.Record
 import java.text.DateFormat
 import java.util.*
 
@@ -11,5 +13,13 @@ object BindingAdapter {
     @JvmStatic
     fun setTime(view: TextView, time: Long) {
         view.text = DateFormat.getDateTimeInstance().format(Date(time))
+    }
+
+    @BindingAdapter("records")
+    @JvmStatic
+    fun setRecords(view: TextView, records: List<Record>) {
+        val diff = if (records.count() > 1) (records[records.lastIndex].time - records[records.lastIndex - 1].time) /
+                (1000 * 60 * 60 * 24) else 0
+        view.text = view.context.getString(R.string.day_format, diff)
     }
 }
--- app/src/main/java/net/yuzumone/recordachi/view/EventAdapter.kt
+++ app/src/main/java/net/yuzumone/recordachi/view/EventAdapter.kt
@@ -5,15 +5,30 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import net.yuzumone.recordachi.R
 import net.yuzumone.recordachi.databinding.ItemEventBinding
-import net.yuzumone.recordachi.model.Event
+import net.yuzumone.recordachi.model.EventCategoryRecords
 
-class EventAdapter : BindingRecyclerAdapter<Event, ItemEventBinding>() {
+class EventAdapter(checkClickCallback: CheckClickCallback) : BindingRecyclerAdapter<EventCategoryRecords,
+        ItemEventBinding>() {
+
+    private var callback: CheckClickCallback = checkClickCallback
+
     override fun createBinding(parent: ViewGroup): ItemEventBinding {
-        return DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.item_event,
-                parent, false)
+        val binding = DataBindingUtil.inflate<ItemEventBinding>(LayoutInflater.from(parent.context),
+                R.layout.item_event, parent, false)
+        binding.buttonCheck.setOnClickListener {
+            val event = binding.event
+            if (event != null) {
+                callback.onCheck(event)
+            }
+        }
+        return binding
     }
 
-    override fun bind(binding: ItemEventBinding, item: Event) {
+    override fun bind(binding: ItemEventBinding, item: EventCategoryRecords) {
         binding.event = item
+    }
+
+    interface CheckClickCallback {
+        fun onCheck(event: EventCategoryRecords)
     }
 }
