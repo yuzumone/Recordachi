a12947b0e67f34d43961ae5b577d1e5c7c527943
yuzumone <yuzumone@outlook.com>
2018-01-21 13:27:59 +0900

    Update main activity

--- app/src/main/java/net/yuzumone/recordachi/MainActivity.kt
+++ app/src/main/java/net/yuzumone/recordachi/MainActivity.kt
@@ -2,11 +2,36 @@ package net.yuzumone.recordachi
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import net.yuzumone.recordachi.fragment.EventListFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportFragmentManager.addOnBackStackChangedListener {
+            if (supportFragmentManager.backStackEntryCount > 0) {
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            } else {
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+            }
+        }
+        if (savedInstanceState == null) {
+            val fragment = EventListFragment()
+            supportFragmentManager.beginTransaction().add(R.id.container, fragment).commit()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
