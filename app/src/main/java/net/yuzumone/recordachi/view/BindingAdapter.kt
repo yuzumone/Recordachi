270dc91e99c7ca9a4f7777e53e06d85e8c503aba
yuzumone <yuzumone@outlook.com>
2018-01-26 10:34:13 +0900

    Update layout

--- app/src/main/java/net/yuzumone/recordachi/view/BindingAdapter.kt
+++ app/src/main/java/net/yuzumone/recordachi/view/BindingAdapter.kt
@@ -1,6 +1,8 @@
 package net.yuzumone.recordachi.view
 
 import android.databinding.BindingAdapter
+import android.graphics.BitmapFactory
+import android.widget.ImageView
 import android.widget.TextView
 import net.yuzumone.recordachi.R
 import net.yuzumone.recordachi.model.Record
@@ -21,5 +23,13 @@ object BindingAdapter {
         val diff = if (records.count() > 1) (records[records.lastIndex].time - records[records.lastIndex - 1].time) /
                 (1000 * 60 * 60 * 24) else 0
         view.text = view.context.getString(R.string.day_format, diff)
+    }
+
+    @BindingAdapter("header")
+    @JvmStatic
+    fun setHeader(view: ImageView, array: ByteArray?) {
+        if (array == null) return
+        val bitmap = BitmapFactory.decodeByteArray(array, 0, array.size)
+        view.setImageBitmap(bitmap)
     }
 }
